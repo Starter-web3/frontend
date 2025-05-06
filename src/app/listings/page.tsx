657e0240b816aa997ec5aa764b0075f91a0bcf73
unknown <bakarezainab74@gmail.com>
2025-05-06 11:06:45 +0100

    Addition of airdrop-listing in the dashboard and adding the dashboard layout

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -6,6 +6,7 @@ import Pagination from '@/src/app/components/propertyListing/Pagination';
 export default function Home() {
   return (
     <main className='min-h-screen bg-gradient-to-b from-blue-900/20 to-purple-900/40'>
+    
       <Header />
       {/* Hero Section */}
       <HeroSection />
